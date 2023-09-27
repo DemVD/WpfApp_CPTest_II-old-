18a0ba491378b01ca37963aec30d6ee4eb56adfc
DemVD <vladimirdemidov19@gmail.com>
2023-09-27 21:21:30 +0300

    Add project files.

---
+++ AppWindows/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+
+namespace WpfApp_CPTest_II.AppWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Создаем колонки
+            DataGridTextColumn column1 = new DataGridTextColumn();
+            column1.Header = "ИНН";
+            column1.Binding = new Binding("Column1");
+
+            DataGridTextColumn column2 = new DataGridTextColumn();
+            column2.Header = "Наименование";
+            column2.Binding = new Binding("Column2");
+
+            DataGridTextColumn column3 = new DataGridTextColumn();
+            column3.Header = "Город";
+            column3.Binding = new Binding("Column3");
+
+            DataGridTextColumn column4 = new DataGridTextColumn();
+            column4.Header = "Область";
+            column4.Binding = new Binding("Column4");
+
+            // Добавляем колонки и устанавливаем источник данных для DataGrid
+            DataGridMainMenu.Columns.Add(column1);
+            DataGridMainMenu.Columns.Add(column2);
+            DataGridMainMenu.Columns.Add(column3);
+            DataGridMainMenu.Columns.Add(column4);
+        }
+
+        public class DataItem
+        {
+            public string Column1 { get; set; }
+            public string Column2 { get; set; }
+            public string Column3 { get; set; }
+            public string Column4 { get; set; }
+        }
+
+        // Проверка DataGrid Заполнением пустыми данными
+        private void ButtonTestData_Click(object sender, RoutedEventArgs e)
+        {
+            // Существующие колонки удалить, если они существуют
+            //DataGridMainMenu.ItemsSource = null;
+            //DataGridMainMenu.Columns.Clear();
+
+            List<DataItem> dataItemList = new List<DataItem>{
+                new DataItem { Column1 = "Значение1_1", Column2 = "Значение1_2", Column3 = "Значение1_3", Column4 = "Значение1_4"},
+                new DataItem { Column1 = "Значение2_1", Column2 = "Значение2_2", Column3 = "Значение2_3", Column4 = "Значение2_4"},
+                new DataItem { Column1 = "Значение3_1", Column2 = "Значение3_2", Column3 = "Значение3_3", Column4 = "Значение3_4"}
+            };
+            DataGridMainMenu.ItemsSource = dataItemList;
+        }
+
+        // clear datagrid
+        private void ButtonClearData_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridMainMenu.ItemsSource = null;
+            DataGridMainMenu.DataContext = null;
+        }
+
+        private void ButtonQuitDB_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonOpenedCompanies_Click(object sender, RoutedEventArgs e)
+        {
+            FetchDataFromDB(true);
+        }
+
+        private void ButtonClosedCompanies_Click(object sender, RoutedEventArgs e)
+        {
+            FetchDataFromDB(false);
+        }
+
+        private void FetchDataFromDB(bool Opened)
+        {
+            CompanyDataAccess CDA =  new CompanyDataAccess();
+            List<DataItem> dataItemList = new List<DataItem>();
+
+            if (Opened)
+            {
+                dataItemList = Convert_ListOfListsStr2ListDataItem(CDA.GetOpenCompanies());
+            }
+            else
+            {
+                dataItemList = Convert_ListOfListsStr2ListDataItem(CDA.GetClosedCompanies());
+            }
+
+            DataGridMainMenu.ItemsSource = dataItemList;
+        }
+
+        private List<DataItem> Convert_ListOfListsStr2ListDataItem(List<List<string>> LL)
+        {
+            List<DataItem> dataItemList = new List<DataItem>();
+            foreach(List<string> L in LL)
+            {
+                DataItem di = new DataItem { Column1 = L[0], Column2 = L[1], Column3 = L[2], Column4 = L[3] };
+                dataItemList.Add(di);
+            }
+            return dataItemList;
+        }
+    }
+}
---
+++ DBInteraction.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace WpfApp_CPTest_II
+{
+    // Представляет таблицу в БД
+    public class CompanyTableCustom
+    {
+        public string ИНН { get; set; }
+        public string Наименование { get; set; }
+        public string Город { get; set; }
+        public string Область { get; set; }
+        public bool Открыт { get; set; }
+    }
+
+    public class CompanyDbContext : DbContext
+    {
+        public DbSet<CompanyTableCustom> Companies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=LAPTOP-K6VSE9NR;Database=CompanyDB;Integrated Security=true;Encrypt=true;TrustServerCertificate=true");
+            }
+        }
+
+        // needed for keyless values
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CompanyTableCustom>().HasNoKey();
+            modelBuilder.Entity<CompanyTableCustom>().ToTable("CompanyTableCustom", "dbo");
+        }
+    }
+
+    public class CompanyDataAccess
+    {
+        private readonly CompanyDbContext _dbContext;
+
+        public CompanyDataAccess()
+        {
+            // Создаем экземпляр контекста базы данных
+            _dbContext = new CompanyDbContext();
+        }
+
+        // Метод для получения списка всех компаний из базы данных
+        public List<CompanyTableCustom> GetAllCompanies()
+        {
+            return _dbContext.Companies.ToList();
+        }
+
+        // Метод для получения компании по её ИНН
+        public CompanyTableCustom GetCompanyByINN(string inn)
+        {
+            return _dbContext.Companies.FirstOrDefault(c => c.ИНН == inn);
+        }
+
+        // Получение массива открытых компаний
+        public List<List<string>> GetOpenCompanies()
+        {
+            List<CompanyTableCustom> openCompanies = _dbContext.Companies.Where(c => c.Открыт).ToList();
+            return openCompanies.Select(c => new List<string> { c.ИНН, c.Наименование, c.Город, c.Область }).ToList();
+        }
+
+        // Получение массива закрытых компаний
+        public List<List<string>> GetClosedCompanies()
+        {
+            List<CompanyTableCustom> closedCompanies = _dbContext.Companies.Where(c => !c.Открыт).ToList();
+            return closedCompanies.Select(c => new List<string> { c.ИНН, c.Наименование, c.Город, c.Область }).ToList();
+        }
+    }
+}
